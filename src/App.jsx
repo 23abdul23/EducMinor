da75934c1dad6ddb3754c106f48a0dd2b463e3cf
adith-nr <adithreganti@gmail.com>
2025-11-15 12:00:28 +0530

    NavBar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Certificates from './views/Certificates'
 import Home from './views/Home'
+import Issue from './views/Issue'
+import Retrieve from './views/Retrieve'
 
-const App = () => <Home />
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin/certificates" element={<Certificates />} />
+      <Route path="/admin/issue-certificate" element={<Issue />} />
+      <Route path="/admin/retrieve-certificate" element={<Retrieve />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
+}
 
 export default App
